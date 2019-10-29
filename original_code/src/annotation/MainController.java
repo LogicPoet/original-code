1680c64c97fe41c99fd640cb724ec07c647e5af8
LogicPoet <2659712343@qq.com>
2019-10-29 20:05:48 +0800

    注解初步了解

---
+++ original_code/src/annotation/MainController.java
@@ -0,0 +1,25 @@
+package annotation;
+
+/**
+ * <p>Title: MainController</p>
+ * <p>description: </p>
+ * <p>Company: </p>
+ *
+ * @author LogicPoet
+ * @version V1.0
+ * @date 2019/9/23 20:39
+ **/
+@Controller
+@RequestMapping(value = "/main")
+public class MainController {
+
+    @RequestMapping(value = "/login")
+    public void login(){
+        System.out.println("login successfully");
+    }
+
+    @RequestMapping(value = "/logout")
+    public void logout(){
+        System.out.println("logout successfully");
+    }
+}
---
+++ original_code/src/annotation/TestMeta.java
@@ -0,0 +1,70 @@
+
+package annotation;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * <p>Title: TestMeta</p>
+ * <p>description: </p>
+ * <p>Company: </p>
+ *
+ * @author LogicPoet
+ * @version V1.0
+ * @date 2019/9/21 16:19
+ **/
+
+public class TestMeta {
+    @MetaAnnotation
+    public TestMeta() {
+    }
+
+    public void elementType() {
+
+    }
+
+    public static void main(String[] args) {
+        //System.out.println(anInt());
+        sun();
+
+        int a=1;
+        a=a++;
+        int  b=a;
+        System.out.println(b);
+    }
+
+
+
+
+    private static int anInt() {
+        int a = 0;
+        try {
+            int i=1/0;
+            System.out.println("执行try");
+            return a;
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("catch执行");
+            return a;
+        } finally {
+            System.out.println("finally执行");
+            System.out.println(a);
+            return a;
+        }
+    }
+
+    private static void sun(){
+        int i=1;//1
+        i=i++;//1
+        System.out.println(i);
+        int j=i++;//j=1
+        System.out.println("--"+i);
+        int k=i+ ++i * i++;//
+        System.out.println("i= "+i);//4
+        System.out.println("j= "+j);//3
+        System.out.println("k= "+k);//15
+         //
+    }
+}
